ee070433b5d09929c92e312bb902eb046184d896
Jimmy Wong <102245488+agi-singularity@users.noreply.github.com>
2024-04-04 15:45:25 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -62,10 +62,10 @@ from pyecharts.faker import Faker
 
 c = (
     Map()
-    .add("商家A", [list(z) for z in zip(Faker.country, Faker.values())], "world")
+    .add("A", [list(z) for z in zip(Faker.country, Faker.values())], "world")
     .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
     .set_global_opts(
-        title_opts=opts.TitleOpts(title="Map-世界地X"),
+        title_opts=opts.TitleOpts(title="Map-X"),
         visualmap_opts=opts.VisualMapOpts(max_=200),
     )
     .render("map_world.html")
