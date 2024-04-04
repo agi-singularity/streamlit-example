400b4f9c498fb2b4f88e543cb864769b6fce2231
Jimmy Wong <102245488+agi-singularity@users.noreply.github.com>
2024-04-04 16:13:33 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -14,11 +14,84 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
+
 import base64
+from io import StringIO
+
+import matplotlib.pyplot as plt
+import streamlit as st
+from matplotlib import numpy as np
 import xml.etree.ElementTree as ET
+
 svg = 'opg1_red_rgb_alz_p0405.svg'
 tree = ET.parse('opg1_red_rgb_alz_p0405.svg')
 root = tree.getroot()
+
+def svg_write(fig, center=True):
+    """
+    Renders a matplotlib figure object to SVG.
+    Disable center to left-margin align like other objects.
+    """
+    # Save to stringIO instead of file
+    imgdata = StringIO()
+    fig.savefig(imgdata, format="svg")
+
+    # Retrieve saved string
+    imgdata.seek(0)
+    svg_string = imgdata.getvalue()
+
+    # Encode as base 64
+    b64 = base64.b64encode(svg_string.encode("utf-8")).decode("utf-8")
+
+    # Add some CSS on top
+    css_justify = "center" if center else "left"
+    css = '<p style="text-align:center; display: flex; justify-content: {};">'.format(css_justify)
+    html = r'{}<img src="data:image/svg+xml;base64,{}"/>'.format(
+        css, b64
+    )
+
+    # Write the HTML
+    st.write(html, unsafe_allow_html=True)
+
+def svg_display(svg_string):
+    """
+    Renders a matplotlib figure object to SVG.
+    Disable center to left-margin align like other objects.
+    """
+   
+    # Encode as base 64
+    b64 = base64.b64encode(svg_string.encode("utf-8")).decode("utf-8")
+
+    # Add some CSS on top
+    css_justify = "center" if center else "left"
+    css = '<p style="text-align:center; display: flex; justify-content: {};">'.format(css_justify)
+    html = r'{}<img src="data:image/svg+xml;base64,{}"/>'.format(
+        css, b64
+    )
+
+    # Write the HTML
+    st.write(html, unsafe_allow_html=True)
+    
+if __name__ == "__main__":
+    fig, ax = plt.subplots(nrows=5, ncols=5, figsize=(10, 10))
+    ax = ax.ravel()
+
+    for i in range(len(ax)):
+        x = np.arange(0, np.pi * np.random.randint(2, 10), 0.1)
+        y = np.sin(x)
+
+        ax[i].plot(x, y)
+
+    plt.tight_layout()
+
+    st.subheader("Peasant pixels")
+    st.write(fig)
+
+    st.subheader("Glorious SVG")
+    svg_display(svg)
+
+
 
 def render_svg(svg):
     """Renders the given svg string."""
